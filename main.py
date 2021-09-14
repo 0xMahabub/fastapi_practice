59b653257878692578401c2e38bc7d22626c1e98
Md Mahabub Alam <mahabub212002072@gmail.com>
2021-09-14 23:49:58 +0600

    dynamic routes

--- main.py
+++ main.py
@@ -8,7 +8,17 @@ def index():
     return "Hello! FastAPI"
 
 
-
+# returning dictionary => will be recieved as JSON
 @app.get('/dic')
 def index():
     return { 'msg': { 'name': 'Mahabub' } }
+
+
+# using dynamic route
+@app.get('/r/{id}')
+def index(id):
+    return { 'r': id }
+
+@app.get('/rt/{id}')
+def index(id: int):
+    return { 'r': id }
